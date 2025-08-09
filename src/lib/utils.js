b73e9a848e67970cc6a6f1ffef1f5edf03cb2498
Dee <nana@Dee-2.local>
2025-08-09 15:00:09 +0700

    dd

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -6,11 +6,12 @@ export const generateToken = (userId, res) => {
   });
 
   res.cookie("jwt", token, {
-    maxAge: 7 * 24 * 60 * 60 * 1000, // MS
-    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
-    secure: process.env.NODE_ENV !== "development",
-  });
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 วัน
+  httpOnly: true,
+  sameSite: process.env.NODE_ENV === "development" ? "lax" : "none", // ปรับค่า SameSite
+  secure: process.env.NODE_ENV === "development" ? false : true, // secure เฉพาะ prod (HTTPS)
+});
+
 
   return token;
 };
